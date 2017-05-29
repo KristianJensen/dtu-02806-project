cf63cb4807b40d36004c6dc30144fd27d4990ac5
Kristian Jensen <jensenkristian89@gmail.com>
2017-05-29 16:59:10 +0200

    Flot plot

--- javascript/load_figures.js
+++ javascript/load_figures.js
@@ -1,6 +1,12 @@
 var load_figure_1 = function() {
 	d3.select("#figure-div").html("");
 	
+	d3.select("#description").html("<b>Strain comparison</b><br>Transcription levels of all genes. Use the drop-down menus to select which strains to compare")
+	
+	d3.selectAll(".fig-link")
+		.classed("selected", false)
+	d3.select("#fig1-link").classed("selected", true)
+	
 	// Figure size and margin
 	var w = 600; 
 	var h = 500;
@@ -12,6 +18,14 @@ var load_figure_1 = function() {
 						{ "text": "St3_trans"},
 						{ "text": "St4_trans"},
 						{ "text": "St5_trans"}]
+	
+	var name_lookup = {
+		"St1_trans": "Strain 1",
+	    "St2_trans": "Strain 2",
+	    "St3_trans": "Strain 3",
+	    "St4_trans": "Strain 4",
+	    "St5_trans": "Strain 5"
+	}
 	
 	d3.tsv("data/final_trans_data.tsv", function(dataset) {
 		var body = d3.select("body");
@@ -40,7 +54,7 @@ var load_figure_1 = function() {
 	  .append('option')
 	  .property("selected", function(d){ return d.text == "St1_trans"; })
 	  .attr('value', function (d) { return d.text })
-	  .text(function (d) { return d.text ;})
+		.text(function (d) { return name_lookup[d.text] ;})
 	figure_div.append('br')
 		 
 	  // Select Y-axis Variable
@@ -55,7 +69,7 @@ var load_figure_1 = function() {
 	      .append('option')
 	      .property("selected", function(d){ return d.text == "St2_trans"; })
 	      .attr('value', function (d) { return d.text })
-	      .text(function (d) { return d.text ;})
+			.text(function (d) { return name_lookup[d.text] ;})
 	  figure_div.append('br')
 		  
  	// Defining the scales
@@ -101,28 +115,32 @@ var load_figure_1 = function() {
 	  .attr("transform", "translate (0, "+(h-padding) + ")")
 	  .attr('id','xAxis')
 	  .call(xAxis)
-	.append('text') // X-axis Label
-	  .attr('id','xAxisLabel')
-	  .attr("x", 0.5*w-40)
-	  .attr("y", 5)
-	  .attr('dy','2.5em')
-	  .text('St1_trans')
 		
 	// Y-axis
 	svg.append('g')
 	  .attr('class','axis')
 	  .attr("transform", "translate(" + padding + ",0)")
 	  .attr('id','yAxis')
-	  .call(yAxis)
-	.append('text') // y-axis Label
-	  .attr('id', 'yAxisLabel')
-	  .attr('transform','rotate(-90)')
-	  .attr("x", -0.6*h)
-	  .attr("y", -10)
-	  .attr("dy", "-3em")
-	  .attr("font-size", "14px")
-	  .text('St2_trans')				
+	  .call(yAxis)				
 
+    svg.append("text")
+		.attr("id", "yAxisLabel")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0)
+        .attr("x", -h/2)
+        .attr("dy", "2em")
+        .style("text-anchor", "middle")
+		.style("font-family", "sans-serif")
+        .text("Strain 2");
+		
+    svg.append("text")
+		.attr("id", "xAxisLabel")
+        .attr("y", h-margin)
+        .attr("x", w/2)
+        .attr("dy", "2em")
+        .style("text-anchor", "middle")
+		.style("font-family", "sans-serif")
+        .text("Strain 1");
 		
 	// Creating circles on scatter
 	svg.selectAll("circle")
@@ -139,7 +157,8 @@ var load_figure_1 = function() {
 		})
 		.attr("r", function(d) {
 		return 2})
-		.attr('fill', 'orange')
+		.attr('fill', 'crimson')
+		.attr("opacity", 0.5)
 		.on("mouseover", function(d) {
 			div.transition()
 				.duration(200)
@@ -171,7 +190,7 @@ var load_figure_1 = function() {
 	        .transition().duration(1000)
 	        .call(yAxis)
 	      d3.select('#yAxisLabel') // change the yAxisLabel
-	        .text(value)    
+			  .text(name_lookup[value])    
 	      d3.selectAll('circle') // move the circles
 	        .transition().duration(1000)
 	          .attr('cy',function (d) { if (parseFloat(d[value])>=0.001) {return parseFloat(yScale(d[value]))}
@@ -192,7 +211,7 @@ var load_figure_1 = function() {
 	        .call(xAxis)
 	      d3.select('#xAxisLabel') // change the xAxisLabel
 	        .transition().duration(1000)
-	        .text(value)
+			  .text(name_lookup[value])
 	      d3.selectAll('circle') // move the circles
 	        .transition().duration(1000)
 	          .attr('cx',function (d) { if (parseFloat(d[value])>=0.001) {return parseFloat(xScale(d[value]))}
@@ -216,6 +235,12 @@ var load_figure_2 = function() {
 	var strain_names = ["Strain 1", "Strain 2", "Strain 3", "Strain 4", "Strain 5"]
 	
 	d3.select("#figure-div").html("");
+	
+	d3.select("#description").html("Nu er det figur 2");
+	
+	d3.selectAll(".fig-link")
+		.classed("selected", false)
+	d3.select("#fig2-link").classed("selected", true)
 	
 	var svg = d3.select("#figure-div").append("svg")
 		.attr("height", h)
@@ -286,7 +311,7 @@ var load_figure_2 = function() {
 				return x_pos })
 			.attr("cy", function(d) { return yScale(Math.max(minValue, d["transcription"])) })
 			.attr("r", function(d) { return rScale(Math.abs(Math.log(Math.max(minValue, d["transcription"])))) })
-			.style("fill", "#0000ff")
+			.style("fill", "crimson")
 			.attr("opacity", function(d) { return alphaScale(Math.abs(Math.log(Math.max(minValue, d["transcription"])))) })
 	
 		var good_dots = svg.selectAll("circle.good")
@@ -316,7 +341,7 @@ var load_figure_2 = function() {
 			svg.selectAll("circle.dot")//.selectAll(id_string)
 				.filter(function(dOther) { return d["NCBI_GENE_ID"] == dOther["NCBI_GENE_ID"] })
 				.attr("r", 7)
-				.style("fill", "red")
+				.style("fill", "#B0C4DE")
 				.attr("opacity", 1)
 		})
 		
@@ -328,7 +353,7 @@ var load_figure_2 = function() {
 				.style("opacity", 0)
 			svg.selectAll("circle.dot")//.selectAll(id_string)
 				.filter(function(dOther) { return d["NCBI_GENE_ID"] == dOther["NCBI_GENE_ID"] })
-				.style("fill", "blue")
+				.style("fill", "crimson")
 				.attr("r", function(d) { return rScale(Math.abs(Math.log(Math.max(minValue, d["transcription"])))) })
 				.attr("opacity", function(d) { return alphaScale(Math.abs(Math.log(Math.max(minValue, d["transcription"])))) })
 		})
@@ -356,6 +381,181 @@ var load_figure_2 = function() {
 	})
 }
 
+//
+// Figure 3 ------------------------
+//
+
 var load_figure_3 = function() {
 	d3.select("#figure-div").html("");
+	
+	d3.select("#description").html(
+		"<b>Regression coefficients</b><br>"
+		+ "A linear regression analysis was performed on all strains using transcription levels as predictors and a relevant fitness value as outcome. "
+		+ "The regression coefficients of each gene are shown below. Error bars show the standard error of the estimates."
+	);
+	
+	d3.selectAll(".fig-link")
+		.classed("selected", false)
+	d3.select("#fig3-link").classed("selected", true)
+	
+	var w = 600; 
+	var h = 900;
+	var padding = 80;
+	var tooltip_pad = 10;
+	
+	d3.tsv("data/gene_transciptomics_coefficients.tsv", function(trans_coef) {
+		  console.log(trans_coef);
+		  var trans_coef_dat = trans_coef;
+		  
+		  var maxX = d3.max(trans_coef_dat, function(d) {return parseFloat(d["mean"]) });
+		  var minX = d3.min(trans_coef_dat, function(d) {return parseFloat(d["mean"]) });
+		  var maxY = d3.max(trans_coef_dat, function(d) {return parseFloat(d["stderr"]) });
+		  var minY = d3.min(trans_coef_dat, function(d) {return parseFloat(d["stderr"]) });
+
+		trans_coef_dat.sort(function(x,y) {
+			return d3.ascending(parseFloat(x.mean), parseFloat(y.mean));
+		})
+		
+	 	// Defining the scales
+		var xScale = d3.scaleLinear()
+			.domain([minX, maxX])
+			.range([padding, w - padding])
+			.nice();
+		var yScale = d3.scaleLinear()
+			.domain([0,trans_coef_dat.length])
+			.range([h - padding, padding])
+			.nice();
+   
+		// Defining the axes
+		var xAxis = d3.axisBottom() 
+			.scale(xScale);
+
+		var yAxis = d3.axisLeft() 
+			.scale(yScale);
+		
+		//Create SVG element
+		var svg = d3.select("#figure-div")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);
+		
+		// Add the tooltip area to the webpage
+		/*var tooltip = d3.select("body")
+			.append("div")
+		    .attr("class", "tooltip")
+		    .style("opacity", .8)
+			.style("background-color", "white")
+			.style("position", "absolute")
+			.style("border", "solid black 1px");*/
+		// Define the div for the tooltip
+		var div = d3.select("body").append("div")	
+		    .attr("class", "tooltip")				
+		    .style("opacity", 0);
+		
+		//Create X axis on the plot
+		svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate (0, "+(h-padding) + ")")
+			.call(xAxis)
+	  	  .append("text")
+	        .attr("class", "label")
+	        .attr("x", w/2)
+	        .attr("y", 40)
+	        .style("text-anchor", "middle")
+	        .text("Regression score (mean)");
+			
+		svg.append("text")
+			.attr("x", w/2)
+			.attr("y", h-padding)
+			.style("text-anchor", "middle")
+			.text("Regression estimate")
+			.attr("dy", "2em")
+	
+		//axis for D4
+		/*svg.append("g")
+			.call(d3.axisBottom().scale(xScale))
+			.attr("transform", "translate (0, "+(h-padding) + ")");*/
+	
+		//Create Y axis on the plot
+		/*svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate(" + padding + ",0)")
+			.call(yAxis)
+	  	  .append("text")
+	        .attr("class", "label")
+			.attr("transform", "rotate(-90)")
+	        .attr("y", -30)
+			.attr("x", -h/2)
+	        .style("text-anchor", "middle")
+	        .text("Std. error");*/
+		
+			
+		svg.selectAll("line")
+	        .data(trans_coef_dat)
+	        .enter()
+			.append("line")
+			.attr("x1", function(d) {
+				return xScale(d['lower'])
+			})
+			.attr("x2", function(d){
+				return xScale(d['upper'])
+			})
+			.attr("y1", function(d,i){
+				return yScale(i)
+			})
+			.attr("y2", function (d,i){
+				return yScale(i)
+			})
+			.attr('stroke-width', 0.5 + 'px')
+			.attr('stroke', '#bbbbbb');
+			
+			
+		// Creating circles on scatter
+		svg.selectAll("circle")
+	        .data(trans_coef_dat)
+	        .enter()
+	        .append("circle")
+			.attr("cx", function(d) { 
+				return xScale(d['mean']);
+			})
+			.attr("cy", function(d,i) {
+				//return (h-h*0.2)-((Math.random()*(h*0.85))+1);})
+				return yScale(i)
+			})
+			.attr("r", function(d) { 
+				return 2})
+			.attr('fill', 'crimson')
+			.attr("opacity", 0.5)
+			.on("mouseover", function(d) {
+				div.transition()
+					.duration(200)
+					.style("opacity", .9);
+				div.html("<b>"+d["NCBI_GENE_ID"]+"</b>" + "<br/> Mean: "+d3.format(".2f")(d["mean"]) 
+					+"<br/> Stderr: "+d3.format(".2f")(d["stderr"]))
+	 			   	.style("left", d3.event.pageX + "px")
+	                .style("top", d3.event.pageY + "px")
+					.style("font-size", 11 + "px");
+			})
+			.on("mouseout", function(d) {
+				div.transition()
+					.duration(500)
+				.style("opacity", 0);
+			});
+			
+			d3.select("#submitted").on("click", function(){
+				var lowy = parseFloat(document.getElementById("ylow").value)
+				var highy = parseFloat(document.getElementById("yhigh").value)
+		      	yScale // change the yScale
+		        	.domain([lowy, highy])
+				yAxis.scale(yScale)
+		      	d3.select('#yAxis') // redraw the yAxis
+		        	.transition().duration(1000)
+			    d3.selectAll('circle') // move the circles
+					.data(trans_coef_dat)
+					.change()
+			        .transition().duration(1000)
+			        .attr('cy',function (d,i) { return yScale(i)
+				})
+			});
+   });
 }
